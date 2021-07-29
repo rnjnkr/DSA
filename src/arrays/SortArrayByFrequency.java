3ca44f26a81611aa1f8d6a9612d9b8baa1b20683
Ranjan Kumar <rnjnkr@gmail.com>
2021-07-29 19:49:43 +0530

    sort array by frequency then by minimum element

---
+++ src/arrays/SortArrayByFrequency.java
@@ -0,0 +1,42 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * WAP to sort an array by frequency of elements in ascending order.
+ * If two elements have the same frequency then the smaller element should come first.
+ * int inputArr[] ={10,10,9,9, 8, 5, 5, 5, 5, 1, 1, 1, 1, 3 };
+ * int outputArr[]  = {3, 8, 9, 9, 10, 10, 1, 1, 1, 1, 5, 5, 5, 5};
+ */
+
+public class SortArrayByFrequency {
+    public static void main(String[] args) {
+        int[] input = {10, 10, 9, 9, 8, 5, 5, 5, 5, 1, 1, 1, 1, 3};
+        Map<Integer, Integer> freq = new HashMap<>();
+        Map<Integer, Integer> index = new HashMap<>();
+
+        for (int i = 0; i < input.length; i++) {
+            if (!freq.containsKey(input[i])) {
+                freq.put(input[i], 1);
+                index.put(input[i], i);
+            } else {
+                freq.put(input[i], freq.get(input[i]) + 1);
+            }
+        }
+
+        List<Integer> lists = Arrays.stream(input).boxed().sorted((a, b) -> {
+            int freq1 = freq.get(a);
+            int freq2 = freq.get(b);
+            if (freq1 != freq2) return freq1 - freq2;
+            else {
+                return index.get(b) - index.get(a);
+            }
+        }).collect(Collectors.toList());
+
+        System.out.println(lists);
+    }
+}
